c732f25b01530e61bc3b8d5b16ebc14be186b7e7
judithsanto04-code <judithsanto04@gmail.com>
2025-12-08 23:55:56 +0200

    yes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function generatePoem(response) {
    new Typewriter("#poem", {
-    strings: "Sente August ",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   }); 
+}
+
+  function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey ="48cob4b4888f16d30bbf2ea4b45t0828";
+  let apiURL= 
+  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let context =
+    "You are a  Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a English poem about ${instructionsInput.value}`;
+
+   let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a English poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
