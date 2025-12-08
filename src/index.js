5c22f9f4436742a8e070fae522baad8e9ce3c538
judithsanto04-code <judithsanto04@gmail.com>
2025-12-08 22:17:29 +0200

    added poem

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function generatePoem(event) {
   event.preventDefault();
 
    new Typewriter("#poem", {
-    strings: "Sente August inus sprichit waz die sele minnit, deme wirdit si glich. minnet si irdische dinc so wirdit si irdisch. minnit si Got, so mochte man fragin, wirdit si dam Got spreche ich daz,daz ludite ungelouplich den di zu krankin sin habin und ez nicht fornemen mer sente Augustinus sprichit ich inspreche ez nicht Iner ich wise uch an di schrift di da sprichit ich habe gesprochin daz ir Gode sit",
+    strings: "Sente August ",
     autoStart: true,
     delay: 1,
     cursor: "",
